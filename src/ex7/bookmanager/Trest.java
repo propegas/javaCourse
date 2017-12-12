3f42f25e3098b0d6ac7cd62339172b22b0b96ad9
macuser <vadim-pegas@ya.ru>
2017-12-12 20:39:18 +0300

    updates

---
+++ src/ex7/bookmanager/Trest.java
@@ -0,0 +1,18 @@
+package ex7.bookmanager;/**
+ * Created by macuser on 12.12.2017.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Trest extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
